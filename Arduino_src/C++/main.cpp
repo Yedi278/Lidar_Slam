9c17643c9e5a0832e4193e75a0e1736a15894e94
yehan edirisinghe <y.edirisinghemudi@campus.unimib.it>
2024-09-05 00:18:37 +0200

    feat: Add extrapolation to data packet processing

--- Arduino_src/C++/main.cpp
+++ Arduino_src/C++/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
 
     p = sh.read_data_packet();
     if(p._is_valid()){
+      p.extrapolate();
       // std::cout << static_cast<int>(p.angle[0] - 0xa0) << std::endl;
       for(int i=0; i<4; i++){
         std::cout << "Angle: " << p.angle[i] << " Distance: " << p.distance[i] << " Signal Strenght: " << p.signal_strength[i] << std::endl;
